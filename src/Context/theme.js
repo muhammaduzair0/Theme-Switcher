b2ddf7c96e97f26a6ea9e05a3c2d468fbd0137e9
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-05-09 19:41:56 +0500

    Added Provider & useContext and export

--- src/Context/theme.js
+++ src/Context/theme.js
@@ -5,3 +5,10 @@ export const ThemeContext = createContext({
   darkTheme: () => {},
   lightTheme: () => {},
 });
+
+
+export const ThemeProvider = ThemeContext.Provider
+
+export default function useTheme(){
+    return useContext(ThemeContext)
+}
