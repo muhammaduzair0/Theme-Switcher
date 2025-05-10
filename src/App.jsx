2943e11b102179802eca06be256a4dc7fb574c51
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-05-10 13:03:52 +0500

    Fixed Theme Provider Values

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
   }, [themeMode]);
 
   return (
-    <ThemeProvider value={{ darkTheme, lightTheme, themeMode }}>
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme }}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
